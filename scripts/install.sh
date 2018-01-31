c2e12f238bd5570385484ec21799649122a936bb
Volker Killesreiter <killesre@gmail.com>
2018-01-31 18:19:23 +0100

    add test

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # create thunder project
-composer create-project burdamagazinorg/thunder-project:2.x ~/builds/thunder --stability dev --no-interaction --no-install
+composer create-project burdamagazinorg/thunder-project:2.x ${THUNDER} --stability dev --no-interaction --no-install
 
-cd ~/builds/thunder
+cd ${THUNDER}
 # Drush 8 is needed as long as there is no drush 9 command version for image-derive-all
 # This actually does a 'composer install'
 composer require drush/drush:~8.1
@@ -12,10 +12,10 @@ composer require drush/drush:~8.1
 cd ~/builds
 git clone --depth=50 https://github.com/BurdaMagazinOrg/theme-thunder-admin.git -b 8.x-2.x
 # link theme
-rm -rf ~/builds/thunder/docroot/themes/contrib/thunder_admin/
-ln -s ~/builds/theme-thunder-admin ~/builds/thunder/docroot/themes/contrib/thunder_admin
+rm -rf ${THUNDER}/docroot/themes/contrib/thunder_admin/
+ln -s ~/builds/theme-thunder-admin ${THUNDER}/docroot/themes/contrib/thunder_admin
 
 #drush site-install standard install_configure_form.enable_update_status_module=NULL
 # /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`"
-cd ~/builds/thunder/docroot
-~/builds/thunder/bin/drush site-install thunder --account-pass=admin --db-url=mysql://thunder:thunder@127.0.0.1/drupal install_configure_form.enable_update_status_module=NULL -y
+cd ${THUNDER}/docroot
+${THUNDER}/bin/drush site-install thunder --account-pass=admin --db-url=mysql://thunder:thunder@127.0.0.1/drupal install_configure_form.enable_update_status_module=NULL -y
---
+++ scripts/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd ${THEME}
+yarn run styles
