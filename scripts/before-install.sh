811bc161c002625d0ffc5426aa6afe0c867ddfaf
Volker Killesreiter <killesre@gmail.com>
2018-01-31 10:30:32 +0100

    add travis.yml, before install script and gitignore

---
+++ scripts/before-install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# add recent nodejs
+source ~/.nvm/nvm.sh
+nvm install stable
+
+# remove xdebug to make php execute faster
+phpenv config-rm xdebug.ini
+
+
+# show php modules
+php -m
+
+
+# Prepare MySQL user and database
+mysql -e "CREATE DATABASE drupal;"
+mysql -e "CREATE USER 'thunder'@'localhost' IDENTIFIED BY 'thunder';"
+mysql -e "GRANT ALL ON drupal.* TO 'thunder'@'localhost';"
