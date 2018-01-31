58558f4d520d3c61b26c4d953404441cb457978c
Volker Killesreiter <killesre@gmail.com>
2018-01-31 15:07:31 +0100

    dirs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 # create thunder project
-composer create-project burdamagazinorg/thunder-project:2.x ~/thunder --stability dev --no-interaction --no-install
+composer create-project burdamagazinorg/thunder-project:2.x ~/builds/thunder --stability dev --no-interaction --no-install
 
 
 # Drush 8 is needed as long as there is no drush 9 command version for image-derive-all
 composer require drush/drush:~8.1
 
 # setup theme
-cd ~
+cd ~/builds
 git clone https://github.com/BurdaMagazinOrg/theme-thunder-admin.git -b 8.x-2.x
-rm -rf ~/thunder/docroot/themes/contrib/thunder_admin/
-ln -s ~/theme-thunder-admin ~/thunder/docroot/themes/contrib/thunder_admin
+ls
+rm -rf ~/builds/thunder/docroot/themes/contrib/thunder_admin/
+ln -s ~/builds/theme-thunder-admin ~/builds/thunder/docroot/themes/contrib/thunder_admin
 
 #drush site-install standard install_configure_form.enable_update_status_module=NULL
 # /usr/bin/env PHP_OPTIONS="-d sendmail_path=`which true`"
