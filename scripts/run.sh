f8859873db57c9efe389ed37b7744a0ba4013c77
Volker Killesreiter <killesre@gmail.com>
2018-02-01 14:33:23 +0100

    add chrome

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#echo '' > ${THUNDER}/docroot/core/themes/seven/css/components/dialog.css
-
 cd ${THEME}
 
 # Run build scripts
@@ -13,9 +11,9 @@ yarn
 git status
 git diff --word-diff=porcelain --exit-code
 
-# Do some linting
+# @todo Do some linting
 
 # Run visual regression tests
-###./node_modules/.bin/sharpeye --single-browser ${SHARPEYE_BROWSER}
+./node_modules/.bin/sharpeye --single-browser ${SHARPEYE_BROWSER}
 
 
