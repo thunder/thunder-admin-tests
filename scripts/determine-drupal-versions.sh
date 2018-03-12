7d3d15ae34bdb0850b277da993428f512d2d9298
Volker Killesreiter <killesre@gmail.com>
2018-03-12 11:53:21 +0100

    add allow failures, fail on missing drupal branch

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -2,7 +2,7 @@
 latest=$(git ls-remote --t https://github.com/drupal/core.git | grep -o 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*$' | awk -F '/' '{ print $3 }' | sort -r | head -1)
 
 branches=( $(git ls-remote -h https://github.com/drupal/core.git |awk -F '/' '{ print $3 }' | grep -e '8' | sort -r) )
-tags=( "8.6.0-alpha" $(git ls-remote -t https://github.com/drupal/core.git |awk -F '/' '{ print $3 }' | grep -e '8' | sort -r))
+tags=( $(git ls-remote -t https://github.com/drupal/core.git |awk -F '/' '{ print $3 }' | grep -e '8' | sort -r))
 branchesToCheck=()
 
 for branch in "${branches[@]}"; do
@@ -12,12 +12,20 @@ for branch in "${branches[@]}"; do
     [[ "${#islatest}" -ne 0 ]] && break
 done
 
+if [ "${DRUPAL}" = "current" ]; then
+  DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | tail -n1)
+fi
 
-[[ ${DRUPAL} = "current" ]] && DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | tail -n1)
-[[ ${DRUPAL} = "next" ]] &&  [[ "${#branchesToCheck}" > 1 ]]&& DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | head -1)
+if [ "${DRUPAL}" = "next" ] && [ "${#branchesToCheck[@]}" > 1 ];then
+  DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | head -1)
+fi
 
 echo "Latest drupal version is ${latest}"
 
-if [ -n "$DRUPAL_BRANCH" ]; then
-    echo "Drupal branch to check ${DRUPAL_BRANCH}"
+
+if [ -z "$DRUPAL_BRANCH" ]; then
+  # Fail if there is no branch.
+  exit 1
+else
+   echo "Drupal branch to check ${DRUPAL_BRANCH}"
 fi
--- scripts/install.sh
+++ scripts/install.sh
@@ -3,14 +3,10 @@
 # Create thunder project
 composer create-project burdamagazinorg/thunder-project:2.x ${HOME}/build/test-dir --stability dev --no-interaction --no-install
 
-if [ ${THUNDER} = "develop" ]; then
+if [ ${THUNDER} = "develop" ] && [ -n "$DRUPAL_BRANCH" ]; then
     cd ${HOME}/build/test-dir
 
-    if [ -n "$DRUPAL_BRANCH" ]; then
-        composer require burdamagazinorg/thunder:dev-develop drupal/core:${DRUPAL_BRANCH}-dev
-    else
-        composer require burdamagazinorg/thunder:dev-develop
-    fi
+    composer require burdamagazinorg/thunder:dev-develop drupal/core:${DRUPAL_BRANCH}-dev
 fi
 
 cd ${HOME}/build/test-dir
