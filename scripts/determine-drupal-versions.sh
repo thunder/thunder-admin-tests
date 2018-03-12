917e5580f063b10e4b566c8cea0ec33e85457aa2
Volker Killesreiter <killesre@gmail.com>
2018-03-12 13:02:13 +0100

    fix return

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 latest=$(git ls-remote --t https://github.com/drupal/core.git | grep -o 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*$' | awk -F '/' '{ print $3 }' | sort -r | head -1)
 
 branches=( $(git ls-remote -h https://github.com/drupal/core.git |awk -F '/' '{ print $3 }' | grep -e '8' | sort -r) )
@@ -13,11 +13,11 @@ for branch in "${branches[@]}"; do
 done
 
 if [ "${DRUPAL}" = "current" ]; then
-  DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | tail -n1)
+  export DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | tail -n1)
 fi
 
 if [ "${DRUPAL}" = "next" ];then
-  [[ "${#branchesToCheck[@]}" -gt 1 ]] && DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | head -1)
+  [[ "${#branchesToCheck[@]}" -gt 1 ]] && export DRUPAL_BRANCH=$(printf -- '%s\n' "${branchesToCheck[@]}" | head -1)
 
   # Fail if there is no branch.
   [[ -z "$DRUPAL_BRANCH" ]] && echo "No tag on next version branch." && exit 1
