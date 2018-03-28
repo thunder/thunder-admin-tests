26f2e9298a78ffd704b81927336e9b859c60be34
Volker Killesreiter <killesre@gmail.com>
2018-03-28 14:55:16 +0200

    re-order

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -5,24 +5,33 @@
 # Use grep since ls-remote refs patterns are not regex.
 refs=( $(git ls-remote https://github.com/drupal/drupal.git 8*) )
 
-# Get latest durpal 8 version.
+# Get latest drupal 8 version.
 latestVersion=$(printf -- '%s\n' ${refs[@]} | grep -o '[0-9]*\.[0-9]*\.[0-9]*$' | tail -1)
 
 # Get branch for current and next minor version
 branchesToCheck=( $(printf -- '%s\n' "${refs[@]}" | grep -A1 ${latestVersion%[0-9]*}x | cut -d/ -f3) )
 
 if [ "${DRUPAL}" = "current" ]; then
+  # Dev branch for latest drupal version.
   DRUPAL_BRANCH=${branchesToCheck[0]}
-elif [ "${DRUPAL}" = "next" ] && [ "${#branchesToCheck}" -gt 1 ];then
-  # Check for tags.
-  gotTag=( $(printf -- '%s\n' ${refs[@]} | grep refs/tags/${branchesToCheck[0]%.x} | cut -d/ -f3) )
-  [[ "${#gotTag}" -ne 0 ]] && export DRUPAL_BRANCH=${branchesToCheck[1]}
+elif [ "${DRUPAL}" = "next" ];then
+
+  if [ "${#branchesToCheck}" -lt 2 ];then
+    echo "No next minor version branch found." && exit 1
+  else
+    # Check for tags on next minov version branch.
+    gotTag=( $(printf -- '%s\n' ${refs[@]} | grep refs/tags/${branchesToCheck[1]%.x} | cut -d/ -f3) )
+
+    if [ "${#gotTag}" -ne 0 ]; then
+      export DRUPAL_BRANCH=${branchesToCheck[1]}
+    else
+      echo "Next minor version branch has no tags." && exit 1
+    fi
+  fi
 fi
 
 if [ -n "$DRUPAL_BRANCH" ]; then
   echo "Drupal branch to check ${DRUPAL_BRANCH}"
-else
-  echo "No branch found." && exit 1
 fi
 
 export DRUPAL_BRANCH
