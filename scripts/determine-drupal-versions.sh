d6dbda94bc2e9c2c99b65ea867e24ad7bfc20be2
Volker Killesreiter <killesre@gmail.com>
2018-03-28 14:09:04 +0200

    reduce to single ls-remote call

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -3,16 +3,21 @@
 # Get latest drupal version.
 # Use github mirror since http://git.drupal.org/project/drupal.git seems to fail quite often.
 # Use grep since ls-remote refs patterns are not regex.
-latestVersion=$(git ls-remote --tags https://github.com/drupal/drupal.git | grep -o 'refs/tags/[0-9]*\.[0-9]*\.[0-9]*$' | cut -d/ -f3 | tail -1)
+refs=( $(git ls-remote https://github.com/drupal/drupal.git 8*) )
+tags=( $(printf -- '%s\n' ${refs[@]} |grep -o 'refs/tags/.*') )
+branches=( $(printf -- '%s\n' ${refs[@]} |grep -o 'refs/heads/.*') )
+
+# Get latest durpal 8 version.
+latestVersion=$(printf -- '%s\n' ${tags[@]} | grep -o '[0-9]*\.[0-9]*\.[0-9]*$' | tail -1)
 
 # Get branch for current and next minor version
-branchesToCheck=( $(git ls-remote -h https://github.com/drupal/drupal.git 8* | cut -d/ -f3 | grep -A1 ${latestVersion%[0-9]*}x ) )
+branchesToCheck=( $(printf -- '%s\n' "${branches[@]}" | grep -A1 ${latestVersion%[0-9]*}x | cut -d/ -f3) )
 
 if [ "${DRUPAL}" = "current" ]; then
   DRUPAL_BRANCH=${branchesToCheck[0]}
 elif [ "${DRUPAL}" = "next" ] && [ "${#branchesToCheck}" -gt 1 ];then
   # Check for tags.
-  gotTag=( $(git ls-remote -t https://github.com/drupal/drupal.git 8* | cut -d/ -f3 | grep ${branchesToCheck[1]%.x}) )
+  gotTag=( $(printf -- '%s\n' ${tags[@]} | grep ${branchesToCheck[0]%.x} | cut -d/ -f3) )
   [[ "${#gotTag}" -ne 0 ]] && export DRUPAL_BRANCH=${branchesToCheck[1]}
 fi
 
