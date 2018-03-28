5ffe071c7b2c7a0a1c2601e7ba0bc070151cc37b
Volker Killesreiter <killesre@gmail.com>
2018-03-28 13:32:57 +0200

    reduce structure

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -10,19 +10,16 @@ branchesToCheck=( $(git ls-remote -h https://github.com/drupal/drupal.git 8* | c
 
 if [ "${DRUPAL}" = "current" ]; then
   DRUPAL_BRANCH=${branchesToCheck[0]}
-fi
-
-if [ "${DRUPAL}" = "next" ] && [ "${#branchesToCheck}" -gt 1 ];then
+elif [ "${DRUPAL}" = "next" ] && [ "${#branchesToCheck}" -gt 1 ];then
   # Check for tags.
   gotTag=( $(git ls-remote -t https://github.com/drupal/drupal.git 8* | cut -d/ -f3 | grep ${branchesToCheck[1]%.x}) )
   [[ "${#gotTag}" -ne 0 ]] && export DRUPAL_BRANCH=${branchesToCheck[1]}
-
-  # Fail if there is no branch.
-  [[ -z "$DRUPAL_BRANCH" ]] && echo "No tag on next minor version branch." && exit 1
 fi
 
 if [ -n "$DRUPAL_BRANCH" ]; then
- echo "Drupal branch to check ${DRUPAL_BRANCH}"
+  echo "Drupal branch to check ${DRUPAL_BRANCH}"
+else
+  echo "No branch found." && exit 1
 fi
 
 export DRUPAL_BRANCH
