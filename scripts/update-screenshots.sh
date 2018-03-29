80f18cad78632051f18a3706d1e6f8f493d06c2e
Volker Killesreiter <killesre@gmail.com>
2018-03-29 15:19:09 +0200

    push

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -17,14 +17,21 @@ if [ ${UPDATE_SCREENSHOTS} == "true" ] && [ ${TRAVIS_PULL_REQUEST}  != 'false' ]
 
     git status
     # Set configuration.
-    git config --global user.email "travis@thunder.org"
-    git config --global user.name "Travis CI"
+    git config --global user.email "technology@thunder.org"
+    git config --global user.name "ThunderTechAccount"
     # Checkout branch.
     git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
     git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
     git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
     # Commit changes.
-    git commit screenshots/screen/* -m 'TRAVIS: Updated visual reference images'
+    git commit screenshots/reference/* -m 'TRAVIS: Updated visual reference images'
     git status
+
+#git config --global credential.helper store
+#echo "https://${GITHUB_TOKEN}:x-oauth-basic@github.com" >> ~/.git-credentials
+
+    git remote set-url origin https://${GITHUB_TOKEN}c@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+    #git remote set-url origin https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+    git push
 fi
 
