9b7b1b8f95d8b82a122ceb2355329072336b2e42
Volker Killesreiter <killesre@gmail.com>
2018-03-29 14:15:19 +0200

    Add handling

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -2,19 +2,24 @@
 
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
-CHANGES=( $(ls screenshots/diff ) )
+if [ ${UPDATE_SCREENSHOTS} == "true" ] && [ ${TRAVIS_PULL_REQUEST}  == 'true' ]; then
+    CHANGES=( $(ls /tmp/sharpeye/${TRAVIS_JOB_ID}/diff ) )
 
-echo "${CHANGES[@]}"
+    if [ "${#CHANGES}" > 0 ]; then
+        for SCREENSHOT in "${CHANGES[@]}"
+        do
+            cp /tmp/sharpeye/${TRAVIS_JOB_ID}/screen/${SCREENSHOT} ./screenshots/reference/
+        done
+    fi
 
-if [[ ${UPDATE_SCREENSHOTS} != "true" ]]; then
-  echo "update screenshots now"
+    git status
+    # Set configuration.
+    git config --global user.email "travis@thunder.org"
+    git config --global user.name "Travis CI"
+    # checkout branch.
+    git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
+    # Commit changes.
+    git commit screenshots/screen/* -m 'TRAVIS: Updated visual reference images'
+    git status
 fi
-
-if ["${TRAVIS_EVENT_TYPE}" == "pull_request" ]; then
-  echo "${TRAVIS_EVENT_TYPE}"
-fi
-
-# Set configuration
-#git config --global user.email "travis@thunder.org"
-#git config --global user.name "Travis CI"
 
