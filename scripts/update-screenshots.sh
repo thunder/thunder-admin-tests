aa4980dc9cb11aa493871ca6a904b5945ad7e321
Volker Killesreiter <killesre@gmail.com>
2018-03-29 16:45:46 +0200

    handle lfs push

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -10,16 +10,27 @@ if [ ${UPDATE_SCREENSHOTS} == "true" ] && [ ${TRAVIS_PULL_REQUEST}  != 'false' ]
 
     if [ "${#CHANGES}" > 0 ]; then
 
-        git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git -b ${TRAVIS_PULL_REQUEST_BRANCH} update
-        cd update
-        echo "screenshots/reference/** filter=lfs diff=lfs merge=lfs -text" > .gitattributes
-        git-lfs pull
-
         for SCREENSHOT in "${CHANGES[@]}"
         do
             cp /tmp/sharpeye/${TRAVIS_JOB_ID}/screen/${SCREENSHOT} ./screenshots/reference/
         done
 
+        git config --global user.email "technology@thunder.org"
+        git config --global user.name "ThunderTechAccount"
+
+        # Reanimate Detached HEAD repo.
+        git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
+        git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
+        git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
+        git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+
+        # Setup lfs for oath token
+        git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.locksverify false
+        git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.access basic
+        git config lfs.url https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
+        git config lfs.pushurl https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
+
+        # Commit and push.
         git status
         git commit screenshots/reference/ -m 'Updated visual reference images'
         git push
