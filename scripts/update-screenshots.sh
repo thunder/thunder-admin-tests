2ac6dc4e43e627ebde5d52faf9d43538e1070d9e
Volker Killesreiter <killesre@gmail.com>
2018-04-03 15:21:18 +0200

    exit early

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -8,29 +8,31 @@ cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 if [ -n "${UPDATE_SCREENSHOTS}" ] && [ "${TRAVIS_PULL_REQUEST_SLUG}" = "BurdaMagazinOrg/theme-thunder-admin" ]; then
 
     CHANGES=( $(ls /tmp/sharpeye/${TRAVIS_JOB_ID}/diff ) )
+
     if [ "${#CHANGES}" > 0 ]; then
-
-        git config --global user.email "technology@thunder.org"
-
-        # Reanimate Detached HEAD repo.
-        git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
-        git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
-        git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
-        git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
-
-        # Setup lfs for oath token
-        git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.locksverify false
-        git config lfs.pushurl https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
-
-        for SCREENSHOT in "${CHANGES[@]}"
-        do
-            cp /tmp/sharpeye/${TRAVIS_JOB_ID}/screen/${SCREENSHOT} ./screenshots/reference/
-            git add ./screenshots/reference/${SCREENSHOT}
-        done
-
-        # Commit and push.
-        git status
-        git commit screenshots/reference/ -m 'TRAVIS: Updated visual reference images'
-        git push
+      exit 0;
     fi
+
+    git config --global user.email "technology@thunder.org"
+
+    # Reanimate Detached HEAD repo.
+    git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
+    git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
+    git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
+    git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+
+    # Setup lfs for oath token
+    git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.locksverify false
+    git config lfs.pushurl https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
+
+    for SCREENSHOT in "${CHANGES[@]}"
+    do
+        cp /tmp/sharpeye/${TRAVIS_JOB_ID}/screen/${SCREENSHOT} ./screenshots/reference/
+        git add ./screenshots/reference/${SCREENSHOT}
+    done
+
+    # Commit and push.
+    git status
+    git commit screenshots/reference/ -m 'TRAVIS: Updated visual reference images for ${SHARPEYE_BROWSER}'
+    git push
 fi
