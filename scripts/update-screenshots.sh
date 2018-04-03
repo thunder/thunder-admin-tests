5e838113a79c65b0f86d7034ff5088236ffdf913
Volker Killesreiter <killesre@gmail.com>
2018-04-03 09:55:21 +0200

    add copied screenshost, move stuff around

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -8,32 +8,30 @@ cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 if [ -n "${UPDATE_SCREENSHOTS}" ] && [ ${TRAVIS_PULL_REQUEST}  != 'false' ]; then
     CHANGES=( $(ls /tmp/sharpeye/${TRAVIS_JOB_ID}/diff ) )
 
+    git config --global user.email "technology@thunder.org"
+
+    # Reanimate Detached HEAD repo.
+    git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
+    git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
+    git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
+    git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
+
+    # Setup lfs for oath token
+    git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.locksverify false
+    git config lfs.pushurl https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
+
     if [ "${#CHANGES}" > 0 ]; then
 
         for SCREENSHOT in "${CHANGES[@]}"
         do
             cp /tmp/sharpeye/${TRAVIS_JOB_ID}/screen/${SCREENSHOT} ./screenshots/reference/
+            git add ./screenshots/reference/${SCREENSHOT}
         done
 
-        git config --global user.email "technology@thunder.org"
-#        git config --global user.name "ThunderTechAccount"
-
-        # Reanimate Detached HEAD repo.
-        git remote set-branches origin ${TRAVIS_PULL_REQUEST_BRANCH}
-        git fetch --depth 1 origin ${TRAVIS_PULL_REQUEST_BRANCH}
-        git checkout ${TRAVIS_PULL_REQUEST_BRANCH}
-        git remote set-url origin https://${GITHUB_TOKEN}@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git
-
-        # Setup lfs for oath token
-        git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.locksverify false
-#        git config lfs.https://github.com/${TRAVIS_PULL_REQUEST_SLUG}.access basic
-#        git config lfs.url https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
-        git config lfs.pushurl https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${TRAVIS_PULL_REQUEST_SLUG}.git/info/lfs
 
         # Commit and push.
         git status
-        git add screenshots/reference/*
-        git commit screenshots/reference/ -m 'Updated visual reference images'
+        git commit screenshots/reference/ -m 'TRAVIS: Updated visual reference images'
         git push
     fi
 fi
