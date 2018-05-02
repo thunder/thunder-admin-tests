b0b501f8f6b2e2feb0d9b5d6de1478588953ecb9
Volker Killesreiter <killesre@gmail.com>
2018-05-02 17:42:05 +0200

    fix handling of whitespace in filenames by setting Internal Field Separator

--- scripts/update-screenshots.sh
+++ scripts/update-screenshots.sh
@@ -11,7 +11,14 @@ if [ -n "${UPDATE_SCREENSHOTS}" ] && [ "${TRAVIS_PULL_REQUEST_SLUG}" = "BurdaMag
       exit 0;
     fi
 
-    CHANGES=( $(ls /tmp/sharpeye/${TRAVIS_JOB_ID}/diff ) )
+    # Temporary set Internal Field Separator.
+    TEMPIFS=$IFS
+    IFS=$'\n'
+
+    CHANGES=( $(find /tmp/sharpeye/${TRAVIS_JOB_ID}/diff -type f) )
+
+    # Restore IFS.
+    IFS=$TEMPIFS
 
     git config --global user.email "technology@thunder.org"
 
