262f1801d3bce0fea5cf2fda1ae2c7d8a6a02f51
Volker Killesreiter <killesre@gmail.com>
2018-10-23 17:19:27 +0200

    Pin chrome 68 (due to viewport resize bug)

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -18,8 +18,8 @@ drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 
 # Run Selenium2 server with Browser relevant for running environment
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
-    # Pin chrome 70 (at random).
-    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-chrome:3.14.0-iron
+    # Pin chrome 68 (due to viewport resize bug).
+    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-chrome:3.14.0-curium
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
     # Use firefox 60 (where esr branches off).
     docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-firefox:3.12.0-cobalt
