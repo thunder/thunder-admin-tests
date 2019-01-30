0fa9076b5e4dd1ab74c977f4f9950e632d90042a
Volker Killesreiter <killesre@gmail.com>
2019-01-30 16:33:42 +0100

    Issue #3029480: Fail on newly created reference issues (#14)

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -5,3 +5,9 @@ cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 # Run visual regression tests
 ./node_modules/.bin/sharpeye --single-browser ${SHARPEYE_BROWSER}
 
+# Fail on newly created reference images
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Failing due to uncommited changes in repo, check your reference images."
+  exit 1
+fi
+
