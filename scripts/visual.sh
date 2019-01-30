9f191b194daf4f93c864aef8627ba15076c2d04c
Volker Killesreiter <killesre@gmail.com>
2019-01-30 14:01:05 +0100

    add check

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -5,3 +5,10 @@ cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 # Run visual regression tests
 ./node_modules/.bin/sharpeye --single-browser ${SHARPEYE_BROWSER}
 
+# Fail on newly created reference images
+if STATUS=$(git status --porcelain) && [ -n "$STATUS" ]; then
+  echo "Failing due to uncommited changes:" 
+  echo "${STATUS}"
+  exit 1
+fi
+
