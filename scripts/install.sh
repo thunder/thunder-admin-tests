0adac8bfd6daeb6d13b7b97213b93268287d4a70
Volker Killesreiter <killesre@gmail.com>
2019-10-23 10:18:50 +0200

    use thunder_styleguide branch fixing dependency

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,7 @@ cd ${HOME}/build/test-dir
 
 # Drush 8 is needed as long as there is no drush 9 command version for image-derive-all
 # this actually does the 'composer install'
-COMPOSER_MEMORY_LIMIT=-1 composer require drush/drush:~8.1 burdamagazinorg/image-derive-all:master@dev thunder/thunder_styleguide thunder/thunder_testing_demo
+COMPOSER_MEMORY_LIMIT=-1 composer require drush/drush:~8.1 burdamagazinorg/image-derive-all:master@dev thunder/thunder_styleguide:fix/thunder-demo-dependency-dev thunder/thunder_testing_demo
 
 # Move theme to destination
 rm -rf ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
@@ -32,4 +32,3 @@ cd ${HOME}/build/test-dir/docroot
 
 # Install thunder
 ${HOME}/build/test-dir/bin/drush site-install thunder --account-pass=admin --db-url=mysql://travis@127.0.0.1/drupal install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
-
