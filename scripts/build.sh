24e7bf8f98ae14d915e8f036fb57f8a69ea42c25
Volker Killesreiter <killesre@gmail.com>
2020-01-14 11:55:48 +0100

    remove nvm from build

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,6 @@
 
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
-# Use node lts version
-set -x
-source ${HOME}/.nvm/nvm.sh
-nvm install --lts
-set +x
-
 # Run build scripts
 node --version
 npm --version
