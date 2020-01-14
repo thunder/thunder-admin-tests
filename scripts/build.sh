e478fda5c85c74794838226f4c6ca829c51510b9
Volker Killesreiter <killesre@gmail.com>
2020-01-14 11:06:26 +0100

    use nvm to get node lts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,10 @@
 
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
+# Use node lts version
+source ${HOME}/nvm/nvm.sh
+nvm install --lts
+
 # Run build scripts
 node --version
 npm --version
