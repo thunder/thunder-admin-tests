95801133c0fdb09c66d77e5fddf901746b2b854c
Volker Killesreiter <killesre@gmail.com>
2020-02-04 13:31:09 +0100

    fix comment

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -21,7 +21,7 @@ if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
     # Pin chrome.
     docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-chrome:3.141.59-zinc
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
-    # Use firefox 64 (nearest to Firefox Quatum 64 ESR).
+    # Use firefox 68 (nearest to Firefox Quatum 68 ESR).
     docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-firefox:3.141.59-titanium
 fi
 
