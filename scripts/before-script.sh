25a9137bdbd8c6d326bc58ed6298310a099f9cd9
Volker Killesreiter <killesre@gmail.com>
2020-02-05 15:32:27 +0100

    Moar memory

--- scripts/before-script.sh
+++ scripts/before-script.sh
@@ -19,10 +19,10 @@ drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 # Run Selenium2 server with Browser relevant for running environment
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
     # Pin chrome.
-    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-chrome:3.141.59-zinc
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
     # Use firefox 68 (nearest to Firefox Quatum 68 ESR).
-    docker run -d -p 4444:4444 --shm-size 256m --net=host selenium/standalone-firefox:3.141.59-titanium
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
 fi
 
 # Show dockers
