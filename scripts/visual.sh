3f4f8d20fade1ae2c84960334c2b5a973c108fb8
Volker Killesreiter <killesre@gmail.com>
2020-02-17 15:17:00 +0100

    lets try native

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -4,13 +4,17 @@
 # Run the webserver
 ${HOME}/build/test-dir/bin/drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 
-# Run Selenium2 server with Browser relevant for running environment
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
     # Pin chrome.
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
+    sudo apt-get install google-chrome-stable
+    npm install -D chromedriver
+    ./node_modules/.bin/geckodriver --host: 127.0.0.1 --port 4444
+
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
     # Use firefox 68 (nearest to Firefox Quatum 68 ESR).
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
+#    sudo apt-get install firefox-esr
+    npm install -D geckodriver
+    ./node_modules/.bin/geckodriver --host: 127.0.0.1 --port 4444
 fi
 
 # Show dockers
