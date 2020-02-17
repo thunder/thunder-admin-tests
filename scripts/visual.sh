4d7ba1c1866f16d3fc680a28738d48eb98a05d32
Volker Killesreiter <killesre@gmail.com>
2020-02-17 14:05:30 +0100

    move docker to visual script

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -1,5 +1,22 @@
 #!/bin/bash -ex
 
+####
+# Run the webserver
+${HOME}/build/test-dir/bin/drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
+
+# Run Selenium2 server with Browser relevant for running environment
+if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
+    # Pin chrome.
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
+elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
+    # Use firefox 68 (nearest to Firefox Quatum 68 ESR).
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
+fi
+
+# Show dockers
+docker ps -a
+####
+
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
 # Run visual regression tests
