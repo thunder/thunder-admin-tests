a0fc920cd5f5ff66bfa034269eecd6fb53ac4e62
Volker Killesreiter <killesre@gmail.com>
2020-02-20 16:05:53 +0100

    silence webdriver

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -12,10 +12,10 @@ export DISPLAY=:99
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
-    chromedriver --port=4444 &
+    chromedriver --port=4444 --silent &
 
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
-    geckodriver --binary /usr/bin/firefox-esr --host 127.0.0.1 --port 4444 &
+    geckodriver --binary /usr/bin/firefox-esr --host 127.0.0.1 --port 4444 &>/dev/null &
 fi
 # Run visual regression tests
 ./node_modules/.bin/sharpeye --single-browser ${SHARPEYE_BROWSER}
