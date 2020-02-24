331194aa0437546e76c06023a4b4355817b01cb9
Volker Killesreiter <killesre@gmail.com>
2020-02-24 15:22:46 +0100

    back to using selenium docker

--- scripts/setup-browser.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
-    # Pin chrome.
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
-elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
-    # Use firefox 68 (nearest to Firefox Quantum 68 ESR).
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
-fi
--- scripts/visual.sh
+++ scripts/visual.sh
@@ -5,6 +5,14 @@ cd ${HOME}/build/test-dir/docroot
 # Start the webserver.
 ${HOME}/build/test-dir/bin/drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 
+if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
+    # Pin chrome.
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
+elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
+    # Use firefox 68 (nearest to Firefox Quantum 68 ESR).
+    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
+fi
+
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
 # Run visual regression tests
