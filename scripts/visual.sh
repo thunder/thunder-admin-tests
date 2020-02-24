045193714744832ef1253d79959986d0152fd2a1
Volker Killesreiter <killesre@gmail.com>
2020-02-24 16:04:42 +0100

    net host

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -ex
 
-cd ${HOME}/build/test-dir/docroot
-
-# Start the webserver.
+####
+# Run the webserver
 ${HOME}/build/test-dir/bin/drush runserver --default-server=builtin 0.0.0.0:8080 &>/dev/null &
 
 if [[ ${SHARPEYE_BROWSER} == "chrome" ]]; then
     # Pin chrome.
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
+    docker run -d --shm-size 2g --net=host selenium/standalone-chrome:3.141.59-zinc
 elif [[ ${SHARPEYE_BROWSER} == "firefox" ]]; then
-    # Use firefox 68 (nearest to Firefox Quantum 68 ESR).
-    docker run -d -p 4444:4444 --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
+    # Use firefox 68 (nearest to Firefox Quatum 68 ESR).
+    docker run -d --shm-size 2g --net=host selenium/standalone-firefox:3.141.59-titanium
 fi
+
+# Show dockers
+docker ps -a
+####
 
 cd ${HOME}/build/test-dir/docroot/themes/contrib/thunder_admin
 
