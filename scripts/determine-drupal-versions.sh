4d87c5eb6f2fb1a8f94fee2613d909e65766f257
Volker Killesreiter <killesre@gmail.com>
2020-02-25 11:18:01 +0100

    no quotes using double brakcets

--- scripts/determine-drupal-versions.sh
+++ scripts/determine-drupal-versions.sh
@@ -11,19 +11,19 @@ latestVersion=$(printf -- '%s\n' "${refs[@]}" | grep -o '[0-9]*\.[0-9]*\.[0-9]*$
 # Get branch for current and next minor version
 branchesToCheck=("$(printf -- '%s\n' "${refs[@]}" | grep -o '[0-9]*\.[0-9]*\.x$' | grep -A1 "${latestVersion%[0-9]*}"x | cut -d/ -f3)")
 
-if [[ "${DRUPAL_BRANCH}" = "current" ]]; then
+if [[ ${DRUPAL_BRANCH} == "current" ]]; then
     # Dev branch for latest drupal version.
     DRUPAL_CORE="${branchesToCheck[0]}"-dev
 
-elif [[ "${DRUPAL_BRANCH}" = "next" ]]; then
+elif [[ ${DRUPAL_BRANCH} == "next" ]]; then
 
-    if [[  "${#branchesToCheck}" -lt 2 ]]; then
+    if [[ ${#branchesToCheck} -lt 2 ]]; then
         echo "No next minor version branch found." && exit 1
     else
         # Check for tags on next minor version branch.
         gotTag=("$(printf -- '%s\n' "${refs[@]}" | grep refs/tags/"${branchesToCheck[1]%.x}" | cut -d/ -f3)")
 
-        if [[ "${#gotTag}" -ne 0 ]]; then
+        if [[ ${#gotTag} -ne 0 ]]; then
             export DRUPAL_CORE="${branchesToCheck[1]}"-dev
         else
             echo "Next minor version branch has no tags." && exit 1
@@ -31,7 +31,7 @@ elif [[ "${DRUPAL_BRANCH}" = "next" ]]; then
     fi
 fi
 
-if [[ -n "${DRUPAL_CORE}" ]]; then
+if [[ -n ${DRUPAL_CORE} ]]; then
     echo "Drupal branch to check ${DRUPAL_CORE}"
 fi
 
--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -4,8 +4,8 @@ cd "${HOME}"/build/test-dir/docroot
 
 # Install thunder
 "${HOME}"/build/test-dir/bin/drush site-install thunder \
-  --account-pass=admin--db-url="sqlite://sites/default/files/.testbasesqlite" \
-  install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
+    --account-pass=admin--db-url="sqlite://sites/default/files/.testbasesqlite" \
+    install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
 
 # Install styleguide
 "${HOME}"/build/test-dir/bin/drush -y en thunder_styleguide
