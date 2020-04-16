521492e5946cab78c7e367abb3f0ed29b4d6155f
Volker Killesreiter <killesre@gmail.com>
2020-04-16 14:02:16 +0200

    Fix drush location

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -3,15 +3,15 @@
 cd "${HOME}"/build/test-dir/docroot
 
 # Install thunder
-"${HOME}"/build/test-dir/bin/drush site-install thunder \
+"${HOME}"/build/test-dir/vendor/bin/drush site-install thunder \
     --account-pass=admin --db-url="sqlite://sites/default/files/.testbasesqlite" \
     install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
 
 # Install styleguide
-"${HOME}"/build/test-dir/bin/drush -y en thunder_styleguide
+"${HOME}"/build/test-dir/vendor/bin/drush -y en thunder_styleguide
 
 # Final cache rebuild, to make sure every code change is respected
-"${HOME}"/build/test-dir/bin/drush cr
+"${HOME}"/build/test-dir/vendor/bin/drush cr
 
 # Pre-create all image styles for entity browser.´
-"${HOME}"/build/test-dir/bin/drush image-derive-all
+"${HOME}"/build/test-dir/vendor/bin/drush image-derive-all
