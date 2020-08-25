c3ed6af7017b51850daf712a6688ee13cf93cc06
Volker Killesreiter <killesre@gmail.com>
2020-08-25 10:18:25 +0200

    enabling and disabling test module discovery does not work

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -8,11 +8,9 @@ cd "${HOME}"/build/test-dir/docroot
     install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
 
 # Install styleguide and disable transitions
-chmod u+w sites/default/settings.php sites/default
-cp sites/default/settings.php /tmp/settings.php
+chmod u+w sites/default/settings.php
 echo "\$settings['extension_discovery_scan_tests'] = TRUE;" >> sites/default/settings.php
 "${HOME}"/build/test-dir/vendor/bin/drush -y en thunder_styleguide css_disable_transitions_test
-mv /tmp/settings.php sites/default/settings.php
 
 # Final cache rebuild, to make sure every code change is respected
 "${HOME}"/build/test-dir/vendor/bin/drush cr
