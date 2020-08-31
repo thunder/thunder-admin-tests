1bd9e7c721e826034d4e054da2057557a88a7ee2
Christian Fritsch <chr.fritsch@gmx.net>
2020-08-31 14:41:23 +0200

    Disable css transitions by enabling a drupal test module

--- scripts/install-application.sh
+++ scripts/install-application.sh
@@ -7,8 +7,10 @@ cd "${HOME}"/build/test-dir/docroot
     --account-pass=admin --db-url="sqlite://sites/default/files/.testbasesqlite" \
     install_configure_form.enable_update_status_module=NULL thunder_module_configure_form.install_modules_thunder_demo -y
 
-# Install styleguide
-"${HOME}"/build/test-dir/vendor/bin/drush -y en thunder_styleguide
+# Install styleguide and disable transitions
+chmod u+w sites/default/settings.php
+echo "\$settings['extension_discovery_scan_tests'] = TRUE;" >> sites/default/settings.php
+"${HOME}"/build/test-dir/vendor/bin/drush -y en thunder_styleguide css_disable_transitions_test
 
 # Final cache rebuild, to make sure every code change is respected
 "${HOME}"/build/test-dir/vendor/bin/drush cr
