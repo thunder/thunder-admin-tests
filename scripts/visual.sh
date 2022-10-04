48eebd85da4c42be3e6cf331b36983cc811702a5
Volker Killesreiter <killesre@gmail.com>
2022-10-04 15:24:09 +0200

    disable php simple server

--- scripts/visual.sh
+++ scripts/visual.sh
@@ -13,7 +13,7 @@ done
 cd "${HOME}"/build/test-dir/docroot/themes/contrib/thunder_admin
 
 # Run the webserver
-php -S 0.0.0.0:8080 -t "${HOME}"/build/test-dir/docroot >/dev/null 2>&1 &
+### php -S 0.0.0.0:8080 -t "${HOME}"/build/test-dir/docroot >/dev/null 2>&1 &
 
 # Run visual regression tests
 if [[ ${UPDATE_SCREENSHOTS} == true ]]; then
